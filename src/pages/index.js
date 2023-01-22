f15408cd52af945d2f3446c0d415c30baebcc0ad
firasel2 <rasel@lemonhive.com>
2023-01-22 12:51:14 +0600

    Gradiant animation created

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 import Head from "next/head";
 
 export default function Home() {
-
   return (
     <>
       <Head>
@@ -12,69 +11,76 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-purple-600 to-rose-700 relative">
-        <h2 className="text-8xl text-center leading-[10rem] font-bold capitalize text-white">
+        <motion.h2
+          animate={{
+            backgroundImage: "linear-gradient(to bottom right,#9333ea,#be123c)",
+            transitionEnd: {
+              backgroundImage: "none",
+            },
+          }}
+          transition={{ duration: 2 }}
+          className="text-5xl md:text-6xl 2xl:text-8xl text-center leading-[4rem] md:leading-[6rem] 2xl:leading-[10rem] font-bold capitalize text-white w-fit relative z-10"
+        >
           Design with purpose,
           <br /> code with passion. <br />
           Keep pushing boundaries,
           <br /> innovate without limits.
-        </h2>
-
-        <div
-          className="absolute h-full w-full top-0 left-0 flex items-center"
-        >
-          <motion.div
-            initial={{
-              backgroundImage:
-                "radial-gradient(circle at 600px 100px, transparent 50px, rgba(0, 0, 0, 1) 100px)",
-            }}
-            animate={{
-              backgroundImage:["radial-gradient(circle at 200px 200px, transparent 50px, rgba(0, 0, 0, 1) 100px)", "radial-gradient(circle at 300px 200px, transparent 50px, rgba(0, 0, 0, 1) 100px)","radial-gradient(circle at 300px 300px, transparent 50px, rgba(0, 0, 0, 1) 100px)","radial-gradient(circle at 500px 500px, transparent 50px, rgba(0, 0, 0, 1) 100px)","radial-gradient(circle at 300px 1500px, transparent 50px, rgba(0, 0, 0, 1) 100px)"],
-              display:"block",
-              transitionEnd:{display:'none'}
-            }}
-            transition={{ duration: 2}}
-            className="w-full h-full hidden"
-          ></motion.div>
-         <motion.div
-            initial={{
-              backgroundImage:
-                "radial-gradient(circle at 500px 500px, transparent 50px, rgba(0, 0, 0, 1) 100px)",
-            }}
-            animate={{
-              backgroundImage:["radial-gradient(circle at 400px 400px, transparent 60px, rgba(0, 0, 0, 1) 150px)", "radial-gradient(circle at 300px 400px, transparent 60px, rgba(0, 0, 0, 1) 150px)","radial-gradient(circle at 300px 300px, transparent 60px, rgba(0, 0, 0, 1) 150px)","radial-gradient(circle at 150px 150px, transparent 60px, rgba(0, 0, 0, 1) 150px)", "radial-gradient(circle at 150px -150px, transparent 60px, rgba(0, 0, 0, 1) 150px)"],
-              display:"block",
-              transitionEnd:{display:'none'}
-            }}
-            transition={{ duration: 2}}
-            className="w-full h-full hidden"
-          ></motion.div>
-          <motion.div
-            initial={{
-              backgroundImage:
-                "radial-gradient(circle at 100px 100px, transparent 50px, rgba(0, 0, 0, 1) 100px)",
-            }}
-            animate={{
-              backgroundImage:["radial-gradient(circle at 200px 200px, transparent 80px, rgba(0, 0, 0, 1) 160px)", "radial-gradient(circle at 300px 200px, transparent 80px, rgba(0, 0, 0, 1) 160px)","radial-gradient(circle at 300px 300px, transparent 80px, rgba(0, 0, 0, 1) 160px)","radial-gradient(circle at 500px 500px, transparent 80px, rgba(0, 0, 0, 1) 160px)", "radial-gradient(circle at 5000px 5000px, transparent 80px, rgba(0, 0, 0, 1) 160px)"],
-              display:"block",
-              transitionEnd:{display:'none'}
-            }}
-            transition={{ duration: 2 }}
-            className="w-full h-full hidden"
-          ></motion.div>
-        </div>
+          <div className="absolute h-full w-full top-0 left-0 flex items-center">
+            <motion.div
+              initial={{
+                backgroundImage:
+                  "radial-gradient(circle at 22% 20%, transparent 10%, rgba(0, 0, 0, 1) 12%)",
+              }}
+              animate={{
+                backgroundImage:
+                  "radial-gradient(circle at 40% 80%, transparent 12%, rgba(0, 0, 0, 1) 20%)",
+                display: "block",
+                transitionEnd: { display: "none" },
+              }}
+              transition={{ duration: 2 }}
+              className="w-full h-full hidden"
+            ></motion.div>
+            <motion.div
+              initial={{
+                backgroundImage:
+                  "radial-gradient(circle at 20% 13%, transparent 10%, rgba(0, 0, 0, 1) 15%)",
+              }}
+              animate={{
+                backgroundImage:
+                  "radial-gradient(circle at 30% 80%, transparent 15%, rgba(0, 0, 0, 1) 20%)",
+                display: "block",
+                transitionEnd: { display: "none" },
+              }}
+              transition={{ duration: 2 }}
+              className="w-full h-full hidden"
+            ></motion.div>
+            <motion.div
+              initial={{
+                backgroundImage:
+                  "radial-gradient(circle at 50% 80%, transparent 10%, rgba(0, 0, 0, 1) 15%)",
+              }}
+              animate={{
+                backgroundImage:
+                  "radial-gradient(circle at 35% 25%, transparent 15%, rgba(0, 0, 0, 1) 25%)",
+                display: "block",
+                transitionEnd: { display: "none" },
+              }}
+              transition={{ duration: 2 }}
+              className="w-full h-full hidden"
+            ></motion.div>
+          </div>
+        </motion.h2>
+        <motion.div
+          initial={{ display: "block" }}
+          animate={{
+            display: "block",
+            background: "#000",
+            transitionEnd: { display: "none" },
+          }}
+          transition={{ duration: 2 }}
+          className="bg-black w-full h-full absolute top-0 left-0 z-[0] hidden"
+        ></motion.div>
       </div>
     </>
   );
-}
-
-{
-  /* <svg width="1440" height="1024" viewBox="0 0 1440 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="1440" height="1024" fill="url(#paint0_radial_1_3)"/>
-<defs>
-<radialGradient id="paint0_radial_1_3" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(331 386) rotate(-98.6649) scale(233.161 231.291)">
-<stop stop-opacity="0"/>
-<stop offset="1"/>
-</radialGradient>
-</defs>
-</svg> */
 }
